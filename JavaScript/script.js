fd5951aa22ef5ee7ad8f8de916d11c11e996b6a9
Mohammad Parvez Musharaf <88303363+parvez2m@users.noreply.github.com>
2022-02-20 13:51:27 +0600

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,72 @@
+// console.log("Bismillahir Rahmanir Rahim");
+
+// Ask me who is the wife of Mohammad Parvez Musharaf
+// let firstName = "FARJANA";
+// let midName = "YEASMIN";
+// let lastName = "ALE";
+// let fullName= firstName + midName + lastName;
+
+// let names = prompt("What is your name ?").toUpperCase();
+// let quis = prompt ("ask me about Mohammad Parvez Musharaf Lover...");
+// let cong=""
+      
+// if(names === fullName){
+      
+// console.log(cong); 
+// }
+
+// else{
+//        console.log(`Hi ${names} we are glad to inform you that our "Mohammad Parvez Musharaf" gonna marry "Farjana Yeasmisn Ale". Acording to Mohammad Parvez Musaharaf, She is the best lady of the world.`)
+// }
+// password must be 6+ character
+
+// const password = prompt("Please enter your password"); 
+// if(password.length>=6){
+//       if(password.indexOf(' ') === -1){
+//             console.log("Valid Password");
+//             }
+//       else {
+//             console.log("password doesn't contain spaces");
+//       }
+            
+//       }
+      
+// else{
+//       console.log("password too short! Must be 6+ Character");
+// }
+      // AMJUMAN ARA BEGUM
+      // MAKBUL HOSSAIN SARKER
+
+function promptMe(){
+
+                  let namee = prompt("What is your name?").toUpperCase();
+                  // let question = prompt("Do want to know who is gonna wife of Md Parvez Mushara? Yes or No").toUpperCase();
+
+                  if(namee === "FARJANA" || namee === "YEASMIN"|| namee === "ALE" || namee==="FARJAN YEASMIN ALE"){
+                        document.write("Hi dear Farjana Yeasmin Ale! Many many congratulations to you. You are wanna be the wife of Mohammad Parvez Musharaf. You are so good we know .Musharaf gonna marry you❤️");
+                  }
+                  else if( namee ==="ANJUMAN" || namee ==="ANJUMAN ARA" || namee === "ANJUMAN ARA BEGUM"){
+                        document.write("আসসালামুয়ালাইকুম আম্মু! মোশারফ ও এলি আপনাকে খুব ভালোবাসেন, তারা আপনার শন্তান হতে পেরে গর্ভিত");
+                  }    
+                  else if(namee === "MAKBUL" || namee ==="MAKBUL HOSSAIN" || namee ==="MAKBUL HOSSAIN SARKER"){
+                        document.write("আসসালামুয়ালাইকুম আব্বু! মোশারফ ও এলি আপনাকে খুব ভালোবাসেন, এক জন শিক্ষক কে বাবা হিসেবে পেয়ে তারা গর্ভিত");
+                  }
+                  else if(namee === "TAWHID" || namee === "TAWHIDUL ISLAM" || namee ==="TAWHIDUL ISLAM TAWHID" || namee ==="NANNU"|| namee==="TAWHIDUL ISLAM NANNU") {
+                        document.write("Founder ans CEO of Tawhid Solution Academy.Father of Tawfiq and  Elder brother of Farjana Yeasmin Ale.")
+                  }
+                  else if(namee === "KHATIJA BEGUM" || namee ==="KHATIJA"){
+                        document.write("Mohther of Mohammad Parvez Musharaf. Your love and prayer makes musharaf's life beautifull")
+                  }
+                  else if(namee === "RASHIDUL ALAM" || namee === "RASHID"){
+                        document.write("We know that you are the father of Mohammad Parvez Musharaf. Your Hardwork and prayer make musharaf's life easy")
+                  }
+                  else if( namee === "RISHAT" || namee === "RISHAT ASHRAF"|| namee === "ASHRAF"){
+                        document.write("You are brother Musharaf")
+                  }
+                  else if (namee === "SABRIN" || namee === "SADIKA AFRIN" || namee === "SADIKA" || namee === "AFRIN"){
+                        document.write("Dear Sadika Afrin! We know about you. you are the sister of Mohammad Parvez Musharaf")
+                   }
+                  else{
+                        document.write("Sorry I dont know...😬")
+                  }
+}
